9c8a518c5158a118bd5147bccb9b6d2b06dd4a8a
dali2g <mamejdi12@gmail.com>
2023-11-21 22:10:04 +0100

    Initial footer added.

--- components/Sections/Footer.tsx
+++ components/Sections/Footer.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import clsx from 'clsx';
 import Link from 'next/link';
 
-interface LastUpdateProps {}
 
-const LastUpdate: React.FC<LastUpdateProps> = () => {
+const LastUpdate: React.FC = () => {
   return (
     <a
       href="https://github.com/dali2g"
@@ -32,7 +30,7 @@ const FooterLink: React.FC<FooterLinkProps> = ({
 }) => {
   if (label === 'soon') {
     return (
-      <span className='flex h-8 items-center gap-2 whitespace-nowrap px-2 py-1 text-sm text-foreground'>
+      <span className={`flex h-8 items-center gap-2 whitespace-nowrap px-2 py-1 text-sm text-foreground`}>
         {title}
         <span className="border-divider-light rounded-full border px-2 py-0 text-[10px] uppercase text-muted-foreground">{label}</span>
       </span>
@@ -41,13 +39,11 @@ const FooterLink: React.FC<FooterLinkProps> = ({
 
   if (isInternal) {
     return (
-    
-        <Link 
-         href={href} passHref
-         className="flex h-8 items-center gap-2 whitespace-nowrap px-2 py-1 text-sm text-foreground">
+      <Link href={href} passHref
+      className={`flex h-8 items-center gap-2 whitespace-nowrap px-2 py-1 text-sm text-foreground`}>
           {title}
           {label && <span className="border-divider-light rounded-full border px-2 py-0 text-[10px] uppercase text-foreground">{label}</span>}
-      
+        
       </Link>
     );
   }
@@ -57,10 +53,10 @@ const FooterLink: React.FC<FooterLinkProps> = ({
       href={href}
       target="_blank"
       rel="noreferrer nofollow"
-      className={clsx('footer-link')}
+      className={`footer-link`}
     >
       {title}
-      {label && <span className={clsx('footer-link__label')}>{label}</span>}
+      {label && <span className={`footer-link__label`}>{label}</span>}
     </a>
   );
 };
@@ -72,16 +68,13 @@ interface FooterGroupProps {
 
 const FooterGroup: React.FC<FooterGroupProps> = ({ title, links }) => {
   return (
-    <div className={clsx('flex-1')}>
+    <div className={`flex-1`}>
       <div
-        className={clsx(
-          'mb-2 px-2 text-[13px] text-slate-600',
-          'dark:text-slate-400'
-        )}
+        className={`mb-2 px-2 text-[13px] text-slate-600 dark:text-slate-400`}
       >
         {title}
       </div>
-      <ul className={clsx('flex flex-col')}>
+      <ul className={`flex flex-col`}>
         {links.map(({ title: linkTitle, href, label, isInternal }) => (
           <li key={href}>
             <FooterLink
@@ -97,31 +90,25 @@ const FooterGroup: React.FC<FooterGroupProps> = ({ title, links }) => {
   );
 };
 
-interface FooterDescriptionProps {}
 
-const FooterDescription: React.FC<FooterDescriptionProps> = () => {
+const FooterDescription: React.FC = () => {
   return (
-    <div className={clsx('max-w-[348px]')}>
+    <div className={`max-w-[348px]`}>
       <div
-        className={clsx(
-          'mb-3 text-[13px] text-slate-600',
-          'dark:text-slate-400'
-        )}
+        className={`mb-3 text-[13px] text-slate-600 dark:text-slate-400`}
       >
         About Me
       </div>
-      <p className={clsx('mb-4 font-normal leading-relaxed')}>
+      <p className={`mb-4 font-normal leading-relaxed`}>
         I&apos;m Mohamed Ali, a <strong>Full Stack Developer</strong> who is here to create innovative and user-friendly web applications.
-
-
       </p>
-      <ul className={clsx('-ml-2 flex gap-1')}>
+      <ul className={`-ml-2 flex gap-1`}>
         <li>
           <a
             href="https://linkedin.com/in/dali2g"
             target="_blank"
             rel="noreferrer nofollow"
-            className={clsx('flex h-9 w-9 items-center justify-center')}
+            className={`flex h-9 w-9 items-center justify-center`}
             aria-label="My Twitter profile"
             title="My Twitter profile"
           >
@@ -133,7 +120,7 @@ const FooterDescription: React.FC<FooterDescriptionProps> = () => {
             href="https://github.com/dali2g"
             target="_blank"
             rel="noreferrer nofollow"
-            className={clsx('flex h-9 w-9 items-center justify-center')}
+            className={`flex h-9 w-9 items-center justify-center`}
             aria-label="My GitHub profile"
             title="My GitHub profile"
           >
@@ -148,24 +135,18 @@ const FooterDescription: React.FC<FooterDescriptionProps> = () => {
 const Footer: React.FC = () => {
   return (
     <footer
-      className={clsx(
-        'background-grid background-grid--fade-in border-divider-light mt-24 pt-16 text-sm text-foreground',
-        'dark:border-divider-dark dark:text-slate-200'
-      )}
+      className={`background-grid background-grid--fade-in border-divider-light mt-24 pt-16 text-sm text-foreground dark:border-divider-dark dark:text-slate-200`}
     >
-      <div className={clsx('content-wrapper')}>
-        <div className={clsx('py-10 font-semibold')}>
-          <div className={clsx('flex flex-col-reverse gap-16', 'lg:flex-row')}>
-            <div className={clsx('flex-1')}>
+      <div className={`content-wrapper`}>
+        <div className={`py-10 font-semibold`}>
+          <div className={`flex flex-col-reverse gap-16 lg:flex-row`}>
+            <div className={`flex-1`}>
               <FooterDescription />
             </div>
             <div
-              className={clsx(
-                '-mx-2 flex flex-1 flex-col gap-8',
-                'sm:flex-row sm:gap-16 lg:mx-0'
-              )}
+              className={`-mx-2 flex flex-1 flex-col gap-8 sm:flex-row sm:gap-16 lg:mx-0`}
             >
-              <div className={clsx('flex', 'sm:gap-16')}>
+              <div className={`flex sm:gap-16`}>
                 <FooterGroup
                   title="Work"
                   links={[
@@ -182,16 +163,12 @@ const Footer: React.FC = () => {
           </div>
         </div>
         <div
-          className={clsx(
-            'border-divider-light flex justify-between border-t py-6 text-xs',
-            'dark:border-divider-dark'
-          )}
+          className={`border-divider-light flex justify-between border-t py-6 text-xs dark:border-divider-dark`}
         >
-          <div className={clsx('font-semibold')}>
+          <div className={`font-semibold`}>
             &copy; 2023, Mohamed Ali Mejdi
           </div>
-          <div className={clsx('text-slate-500', 'dark:text-slate-400')}>
-            <LastUpdate />
+          <div className={`text-slate-500 dark:text-slate-400`}>
           </div>
         </div>
       </div>
